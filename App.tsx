99b235b79c6233fcb11d46ae9e4979b7e4e039fd
Jorge Diaz <jorlup@gmail.com>
2022-02-12 03:01:10 -0500

    add: alias babel y tsconfig

--- App.tsx
+++ App.tsx
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
+import {TransItem} from "@Components/TransItem";
+
+interface IItem {
+  value: string;
+  id: number;
+}
 
 export default function App() {
+  const [textInput, SetTextInput] = useState<string>("");
+  const [itemList, SetItemList] = useState<IItem[]>([]);
+
+  const handleOnPress = () => {
+    if (textInput.length > 0) {
+      SetTextInput("");
+      SetItemList([
+        ...itemList,
+        {
+          value: textInput,
+          id: Math.random(),
+        } as IItem,
+      ]);
+    }
+  };
+
+  const handleOnChangeText = (text: string): void => {
+    SetTextInput(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={handleOnChangeText}
+          value={textInput}
+        />
+        <Button onPress={handleOnPress} title="Add" color="#841584" />
+      </View>
+      <View>
+        <FlatList
+          data={itemList}
+          renderItem={({ item }) => (<TransItem item={item} onPress={undefined} />)}
+          keyExtractor={item => item.id.toString()}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { padding: 50 },
+  inputContainer: {
+    flexDirection: "row",
+  },
+  input: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginRight: 10,
+  },
+  item: {
+    borderColor: "black",
+    borderWidth: 1,
+    marginVertical: 10,
+    padding: 10,
   },
 });
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+import { compilerOptions } from './tsconfig.json';
+let rawAlias   = compilerOptions.paths;
+let alias      = {};
+
+for (let x in rawAlias) {
+    alias[x.replace('/*', '')] = rawAlias[x].replace('/*', '');
+}
+
+export default function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ["module-resolver", {
+        alias,
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+          '.json',
+        ]
+      }],
+    ]
   };
 };
